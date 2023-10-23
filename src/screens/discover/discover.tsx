644b28d0d97e7e1898a9f1168f11f0b31683216e
Jersy Fernandes <102835855+JersyFernandesJF@users.noreply.github.com>
2023-10-23 15:04:20 +0100

    changes

--- src/screens/discover/discover.tsx
+++ src/screens/discover/discover.tsx
@@ -3,7 +3,21 @@ import { Block, Button, Text, Comment, Include, Post } from "~/components";
 export const DiscoverScreen = () => {
   return (
     <Block flex={1} style={{ backgroundColor: "#ffff" }} center>
-      <Text>Discover</Text>
+      <Block
+        row
+        justifyContent="space-between"
+        center
+        style={{
+          width: 290,
+          height: 50,
+          borderRadius: 12,
+          borderWidth: 1,
+          borderColor: "rgba(212, 212, 212, 1)",
+        }}
+      >
+        <Text>09:00 - 09:30</Text>
+        <Text>31,50$</Text>
+      </Block>
     </Block>
   );
 };
